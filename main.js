5f432a8ee4904554554138539d2fac717fe854dd
Hélio Baptista Martins <hbm.helio@gmail.com>
2016-06-23 15:41:41 -0300

    required sei la

--- main.js
+++ main.js
@@ -1,51 +1,95 @@
 var app = angular.module('training', ['ngMaterial']);
 
-    'ng-inject';
+'ng-inject';
 
-    app.controller('ExpansionController', function() {
+app.controller('ExpansionController', function() {
 
-      var self = this;
+        var self = this;
 
-      self.cartItems = [{id:1, name:"a"}, {id:2, name:"b"}];
+        self.duplicatePanel = function() {
+          console.log('panel duplicated');
+        };
+
+        self.deletePanel = function() {
+          console.log('panel deleted');
+        };
+
+        self.obj = {
+          collection: [{
+              id: 1,
+              accountStringFrom: "House Ads",
+              accountStringTo: "Marketing",
+              value: "5000"
+          }, {
+            id: 1,
+            accountStringFrom: "House Ads",
+            accountStringTo: "Marketing",
+            value: "5000"
+          }],
+          mdPanelLabel: 'value',
+          mdPanelHeaderValues: ['accountStringFrom', 'accountStringTo'],
+          mdPanelActions: [{
+              idx: 0,
+              label: 'Delete',
+              action: self.deletePanel,
+              primary: false
+          },
+          {
+            idx: 1,
+            label: 'Duplicate',
+            action: self.duplicatePanel,
+            primary: true
+          }]
+        };
 
     })
     .controller('PanelListCtrl', function() {
 
-      var self = this;
+        var self = this;
 
-      self.activePanel = null;
+        self.activePanel = null;
+
+        self.controlExpandedPanel = function(idx) {
+          //self.activePanel = index;
+          console.log(idx);
+        }
 
     })
     .directive('expansionPanelList', function() {
-      return {
-        scope: {
-          panels: '='
-        },
-        bindToController: true,
-        controller: 'PanelListCtrl',
-        controllerAs: '$list',
-        restrict: 'E',
-        templateUrl: 'expansion-panel-list.html'
-      }
+        return {
+            scope: {
+                panels: '='
+            },
+            bindToController: true,
+            controller: 'PanelListCtrl',
+            controllerAs: '$list',
+            restrict: 'E',
+            templateUrl: 'expansion-panel-list.html'
+        }
     })
     .controller('ExpansionPanelController', function() {
         var self = this;
+        self.isExpanded = false;
     })
     .directive('expansionPanel', function() {
-      return {
-        restrict: 'E',
-        scope: {
-          panel: '=',
-        },
-        bindToController: true,
-        controller: 'ExpansionPanelController',
-        controllerAs: '$item',
-        templateUrl: 'expansion-panel.html'
-      };
+        return {
+            restrict: 'E',
+            scope: {
+                panel: '=',
+                label: '=',
+                attributes: '=',
+                expandHandler: '=',
+                actions: '='
+            },
+            bindToController: true,
+            controller: 'ExpansionPanelController',
+            controllerAs: '$item',
+            templateUrl: 'expansion-panel.html'
+        };
     })
     .directive('expansionPanelContent', function() {
-      return {
-        restrict: 'E',
-        templateUrl: 'expansion-panel-content.html'
-      }
+        return {
+            restrict: 'E',
+            templateUrl: 'expansion-panel-content.html'
+        }
     });
